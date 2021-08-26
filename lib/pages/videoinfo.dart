51d00cf049a2f4aa90cc3e9a6aa4d603f1e7d696
Atanu Debnath <playatanuyt@gmail.com>
2021-08-26 23:25:14 +0530

    vid

--- lib/pages/videoinfo.dart
+++ lib/pages/videoinfo.dart
@@ -21,8 +21,8 @@ class VideoInfo extends StatefulWidget {
 class _VideoInfoState extends State<VideoInfo> {
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    //double width = MediaQuery.of(context).size.width;
+    // double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.black,
